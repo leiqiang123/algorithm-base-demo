b74e361fdf8593540b82cf3c2e624de49bd6c37a
leiqiang123 <123456789>
2021-03-10 21:27:00 +0800

    init

---
+++ webpack_img/webpack.config.js
@@ -0,0 +1,50 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  //loader配置
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          esModule: false,
+          name: '[hash:10].[ext]',
+          outputPath: 'imgs'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    //plugins配置
+    new HtmlWebpackPlugin({
+      //以 './src/index.html' 为模板html，自动引入打包资源
+      template: './src/index.html'
+    })
+  ],
+  //模式
+  mode: 'development',
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    //gzip
+    compress: true,
+    port: 3000,
+    //自动打开浏览器
+    open: true
+  }
+}
